a9f8b9e8b92d6fe02bef1375be8e3d78f1c20579
Ronosavio <ronosavio7@gmail.com>
2025-02-15 00:12:26 +0530

    made some structure to work on for my code

--- complete_linear_regression.py
+++ complete_linear_regression.py
@@ -1,5 +1,57 @@
-import pandas as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+class dataset_analysis():
+     def __init__(self, df):
+         self.df = df
+     def load_data(self):
+         pass
+     
+     def feature_extraction():
+         pass
+     
+     def feature_selection():
+         pass 
+     
+     def target_extraction():
+         pass
+     
+     def target_feature_corelation():
+         pass 
+     
+     def overall_validation():
+         pass
+     
+     def pass_on():
+         pass
+     
+class Model_implementation():
+     def __init__():
+        pass
+    
+     def training():
+        pass
+    
+     def prediction():
+        pass
+    
+     def Z_score_validation():
+         pass
+     
+     def overall_validation():
+         pass
+     
+     def pass_on():
+         pass
+        
+class Plot_mechanics():
+     def __init__(self):
+         pass
+     def Linear_Regression_plot(self):
+         pass
+     
+         
+         
