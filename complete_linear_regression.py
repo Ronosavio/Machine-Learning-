5f3678a788e6cfffc7d3ea920dc7c013511c280b
Ronosavio <ronosavio7@gmail.com>
2025-02-18 00:32:48 +0530

    just a few progress in my logic

--- complete_linear_regression.py
+++ complete_linear_regression.py
@@ -8,16 +8,23 @@ class dataset_analysis():
      def __init__(self, df):
          self.df = df
      def load_data(self):
-         pass
+         loaded_data = pd.read_csv(self.df)
+         return dataset_analysis.feature_target_extraction(loaded_data)
      
-     def feature_extraction():
-         pass
+     def feature_target_extraction(loaded_data):
+         binary_cols = [i for i in loaded_data.columns if loaded_data[i].nunique() == 2]
+         if binary_cols == []:
+            pass
+         else:
+            return'This must be a logistic regression problem as the target is holding binary values'
+         target_value = loaded_data.columns[-1]
+         feature_values = loaded_data.columns[:-1]
+         return target_value, feature_values
+         
      
      def feature_selection():
          pass 
      
-     def target_extraction():
-         pass
      
      def target_feature_corelation():
          pass 
@@ -53,5 +60,5 @@ class Plot_mechanics():
      def Linear_Regression_plot(self):
          pass
      
-         
-         
+analysis = dataset_analysis("D:/DS/CSV_files/Advertising.csv")
+print(analysis.load_data())
